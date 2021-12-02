2e200820318d680886d58e6487c838527a679b7e
manchikooo <manchiko.man31@mail.ru>
2021-12-02 22:52:34 +0300

    doing first homework from ignat

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import Message from "./Message";
+
+export type CompanionsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+type MessageDataType = {
+    companions: CompanionsType
+}
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    companions: [
+        {
+            avatar: 'https://avatars.mds.yandex.net/get-zen_doc/2816669/pub_5ec384fdd2576a75855f6578_5ec6222a5bdd272313d6e24c/scale_1200',
+            name: 'Sakura Haruno',
+            message: 'Если мы умрем, то лучше умереть сражаясь, чем ничего не делая!',
+            time: '21:59',
+        },
+        {
+            avatar: 'https://64.media.tumblr.com/f239b93f4852cf1a7abfbc8595083334/d25cd16f2cf37eeb-20/s500x750/9c2c56a2740ae4774dc7e6430fe5f44ec554db52.jpg',
+            name: 'Hinata Hyuga',
+            message: 'Ты не идеален, но, делая ошибки, ты учишься на них. Мне кажется, настоящая сила заключается именно в этом.',
+            time: '22:03',
+        },
+        {
+            avatar: 'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkPb7cRrNQuuDVsZ1ApWcCk6aKTM5SRkZCeTgDn6uOyic&fn=sqr_288',
+            name: 'Tsunade Senju',
+            message: 'Деньги нужно срочно пропить, так как потом их просто не будет...',
+            time: '22:05',
+        },
+    ]
 }
+
+let companions = messageData.companions.map(c => <Message avatar={c.avatar}
+                                                          name={c.name}
+                                                          message={c.message}
+                                                          time={c.time}/>)
 
 function HW1() {
     return (
@@ -15,12 +48,7 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            <Message
-                avatar={messageData.avatar}
-                name={messageData.name}
-                message={messageData.message}
-                time={messageData.time}
-            />
+            {companions}
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import classes from './Message.module.css'
+import {CompanionsType} from "./HW1";
 
-function Message() {
+export function Message(props: CompanionsType) {
     return (
-        <div>
-
+        <div className={classes.messageBlock}>
+            <div className={classes.avatar}>
+                <img src={props.avatar} className={classes.avatarIcon}/>
+            </div>
+            <div className={classes.messageInfoBlock}>
+                <div className={classes.messageInfo}>
+                    <div className={classes.companionName}>{props.name}</div>
+                    <div>{props.message}</div>
+                </div>
+                <div className={classes.messageTime}>
+                    <div className={classes.messageTime}>{props.time}</div>
+                </div>
+            </div>
         </div>
     )
 }
