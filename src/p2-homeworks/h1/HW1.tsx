91705e1c4e1aeb7430843a36256fa193c65a358b
manchikooo <manchiko.man31@mail.ru>
2021-12-02 23:40:14 +0300

    doing first gomework from ignat

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Message from "./Message";
 
 export type CompanionsType = {
+    id?: never
     avatar: string
     name: string
     message: string
@@ -12,21 +13,24 @@ type MessageDataType = {
     companions: CompanionsType
 }
 
-const messageData = {
+export const messageData = {
     companions: [
         {
+            id: 1,
             avatar: 'https://avatars.mds.yandex.net/get-zen_doc/2816669/pub_5ec384fdd2576a75855f6578_5ec6222a5bdd272313d6e24c/scale_1200',
             name: 'Sakura Haruno',
             message: 'Если мы умрем, то лучше умереть сражаясь, чем ничего не делая!',
             time: '21:59',
         },
         {
+            id: 2,
             avatar: 'https://64.media.tumblr.com/f239b93f4852cf1a7abfbc8595083334/d25cd16f2cf37eeb-20/s500x750/9c2c56a2740ae4774dc7e6430fe5f44ec554db52.jpg',
             name: 'Hinata Hyuga',
             message: 'Ты не идеален, но, делая ошибки, ты учишься на них. Мне кажется, настоящая сила заключается именно в этом.',
             time: '22:03',
         },
         {
+            id: 3,
             avatar: 'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkPb7cRrNQuuDVsZ1ApWcCk6aKTM5SRkZCeTgDn6uOyic&fn=sqr_288',
             name: 'Tsunade Senju',
             message: 'Деньги нужно срочно пропить, так как потом их просто не будет...',
@@ -39,6 +43,10 @@ let companions = messageData.companions.map(c => <Message avatar={c.avatar}
                                                           name={c.name}
                                                           message={c.message}
                                                           time={c.time}/>)
+//
+// if (messageData.companions[0].id === 1) {
+//     return 'messageInfoBlockSakura'
+// }
 
 function HW1() {
     return (
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import classes from './Message.module.css'
-import {CompanionsType} from "./HW1";
+import {CompanionsType, messageData} from "./HW1";
 
 export function Message(props: CompanionsType) {
+
+    // if (messageData.companions[0].id === 1) {
+    //     return ClassName
+    // } else if (messageData.companions[1].id === 2) {
+    //     return ClassName
+    // } else if (messageData.companions[2].id === 3) {
+    //     return ClassName
+    // }
+
+    // КАК СДЕЛАТЬ КАЖДОМУ ПЕРСОНАЖУ СВОЙ ЦВЕТ???
+
     return (
         <div className={classes.messageBlock}>
             <div className={classes.avatar}>
                 <img src={props.avatar} className={classes.avatarIcon}/>
             </div>
-            <div className={classes.messageInfoBlock}>
+            <div className={`${classes.messageInfoBlock} ${classes.messageInfoBlock + props.id}`}>
                 <div className={classes.messageInfo}>
                     <div className={classes.companionName}>{props.name}</div>
                     <div>{props.message}</div>
