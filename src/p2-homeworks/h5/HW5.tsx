b2fd8fe407ef9d64b2509ba7b77dba9c4709b141
manchikooo <manchiko.man31@mail.ru>
2021-12-20 16:39:09 +0300

    making menu

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './Header'
 import {HashRouter} from "react-router-dom";
 import RoutesTo from "./RoutesTo";
+import classes from "./Header.module.css";
 
 function HW5() {
     return (
         <div>
             <HashRouter>
+                <div className={classes.navigation}>
                 <Header/>
+                </div>
                 <RoutesTo/>
             </HashRouter>
         </div>
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
 import classes from './Header.module.css'
 
-function Header() {
+type HeaderPropsType = {}
+
+function Header(props: HeaderPropsType) {
+
+    return (
+        <div className='selectBlock'>
+            <SelectTitle title={'Меню'}/>
+        </div>
+    )
+}
+
+
+type SelectTitleType = {
+    title: string
+}
+
+function SelectTitle(props: SelectTitleType) {
+
+    const [collapsed, setCollapsed] = useState(true)
+
+    return (<div className={classes.navigation}
+            // onBlur={() => setCollapsed(true)}
+                 tabIndex={0}>
+            <div className={classes.navHeader}>
+                <h3 style={{cursor: "pointer"}}
+                    onClick={() => setCollapsed(!collapsed)}
+                >{props.title}</h3>
+                {!collapsed && <SelectBody/>}
+            </div>
+        </div>
+    )
+}
+
+function SelectBody() {
     return (
         <div className={classes.navLinks}>
             <span><NavLink to={'/pre-junior'}>pre-junior</NavLink></span>
             <span><NavLink to={'/junior'}>junior</NavLink></span>
             <span><NavLink to={'/junior-plus'}>junior-plus</NavLink></span>
-        </div>
-    )
+            {/*{props.items.map((i, index) =>*/}
+            {/*    <li onClick={() => props.setSelectValue(i.title)}*/}
+            {/*        key={index}>{i.title}</li>)}*/}
+        </div>)
 }
+
+// return (
+//     <div className={classes.navLinks}>
+//         <span><NavLink to={'/pre-junior'}>pre-junior</NavLink></span>
+//         <span><NavLink to={'/junior'}>junior</NavLink></span>
+//         <span><NavLink to={'/junior-plus'}>junior-plus</NavLink></span>
+//     </div>
+// )
+
 
 export default Header
