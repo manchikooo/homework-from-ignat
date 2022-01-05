860b5969b1643232e02c6ae41a97771dbb2ca280
manchikooo <manchiko.man31@mail.ru>
2022-01-05 17:08:10 +0300

    making hover-effect for editable span

--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -69,7 +69,8 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-                        {children || restProps.value}
+
+                        ✎ ️{children || restProps.value}
                     </span>
                 )
             }
